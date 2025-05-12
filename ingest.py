d8cbb1e2d526e8246bb04bca2ac23e09a4bd06bb
Saba Firdaus Ansaria <92887850+sfansaria@users.noreply.github.com>
2025-05-12 15:49:54 +0100

    Update ingest.py

--- ingest.py
+++ ingest.py
@@ -1,3 +1,5 @@
+#Import the Libraries
+
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
 from langchain_community.document_loaders import PyPDFLoader, DirectoryLoader
@@ -8,19 +10,27 @@ vectorstore_database_path = 'vectorstore/db'
 
 # Create vector database
 def create_vector_db():
+    #Load the Data in the PDF Format
     loader = DirectoryLoader(Data_path,
                              glob='*.pdf',
                              loader_cls=PyPDFLoader)
 
     documents = loader.load()
+
+    #Split the Data
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=50)
     texts = text_splitter.split_documents(documents)
 
+    #Create the Embeddings
     embeddings = HuggingFaceEmbeddings(model_name='sentence-transformers/all-MiniLM-L6-v2',
                                        model_kwargs={'device': 'cpu'})
 
+    #Create the Vectorstore Database using FAISS
     db = FAISS.from_documents(texts, embeddings)
+
+    #Saving the Vectorstore Database locally 
     db.save_local(vectorstore_database_path)
+    
     print("Vector database created successfully.")
 
 if __name__ == "__main__":

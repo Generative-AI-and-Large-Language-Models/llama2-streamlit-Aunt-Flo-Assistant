4d09d9236060bbd4c579909468a18cbaca682f22
Saba Firdaus Ansaria <92887850+sfansaria@users.noreply.github.com>
2025-05-12 15:45:55 +0100

    Add files via upload

---
+++ ingest.py
@@ -0,0 +1,27 @@
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_community.vectorstores import FAISS
+from langchain_community.document_loaders import PyPDFLoader, DirectoryLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter 
+
+Data_path = '/Users/sabafirdausansaria/Downloads/Aunt_Flo_Assistant_Using_Conversational_RAG_LLAMA2/Data'
+vectorstore_database_path = 'vectorstore/db'
+
+# Create vector database
+def create_vector_db():
+    loader = DirectoryLoader(Data_path,
+                             glob='*.pdf',
+                             loader_cls=PyPDFLoader)
+
+    documents = loader.load()
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=50)
+    texts = text_splitter.split_documents(documents)
+
+    embeddings = HuggingFaceEmbeddings(model_name='sentence-transformers/all-MiniLM-L6-v2',
+                                       model_kwargs={'device': 'cpu'})
+
+    db = FAISS.from_documents(texts, embeddings)
+    db.save_local(vectorstore_database_path)
+    print("Vector database created successfully.")
+
+if __name__ == "__main__":
+    create_vector_db()
